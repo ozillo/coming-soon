a55f30bd637f9acc3c10a7b856c6dba71e6a3c8c
ozillo <marcmateo25@gmail.com>
2024-12-04 00:23:16 +0100

    2nd commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div class='black'>
+      <div class="container">
+        COMING
+      </div>
+    </div>
+    <div class="white">
+      <div class="container">
+        SOON
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
